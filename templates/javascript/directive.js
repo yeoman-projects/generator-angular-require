f3d056998861e1654ce469dc275754bd4412503e
Aaron Allport <aaronallport@Aarons-MacBook-Pro.local>
2014-01-21 12:54:17 +0000

    Directive wrapped in define call

--- templates/javascript/directive.js
+++ templates/javascript/directive.js
@@ -1,12 +1,14 @@
-'use strict';
+define(['angular'], function (angular) {
+  'use strict';
 
-angular.module('<%= scriptAppName %>')
-  .directive('<%= cameledName %>', function () {
-    return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the <%= cameledName %> directive');
-      }
-    };
-  });
+  angular.module('<%= scriptAppName %>')
+  	.directive('<%= cameledName %>', function () {
+      return {
+      	template: '<div></div>',
+      	restrict: 'E',
+      	link: function postLink(scope, element, attrs) {
+          element.text('this is the <%= cameledName %> directive');
+      	}
+      };
+  	});
+});
