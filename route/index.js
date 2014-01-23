0502b2afea03569c36c642bc54065779663d48cb
Aaron Allport <aaronallport@Aarons-MacBook-Pro.local>
2014-01-23 10:37:20 +0000

    correcting sub-generator hooks on route generator execution

--- route/index.js
+++ route/index.js
@@ -7,8 +7,8 @@ var angularUtils = require('../util.js');
 
 var Generator = module.exports = function Generator() {
   ScriptBase.apply(this, arguments);
-  this.hookFor('angular:controller');
-  this.hookFor('angular:view');
+  this.hookFor('angular-require:controller');
+  this.hookFor('angular-require:view');
 };
 
 util.inherits(Generator, ScriptBase);
