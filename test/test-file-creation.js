9284cbfdcfaa1bad901f842be23e1dbc37384d89
Aaron Allport <aaronallport@Aarons-MacBook-Pro.local>
2014-01-24 16:58:37 +0000

    Updating describe block text

--- test/test-file-creation.js
+++ test/test-file-creation.js
@@ -8,7 +8,7 @@ var generators = require('yeoman-generator');
 var helpers = require('yeoman-generator').test;
 var _ = require('underscore.string');
 
-describe('Angular generator', function () {
+describe('Angular-RequireJS generator', function () {
   var angular;
 
   beforeEach(function (done) {
