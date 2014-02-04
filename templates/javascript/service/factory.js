0e0f1299a47c85edf63f5020e092bc0f512f53ac
Aaron Allport <aaronallport@Aarons-MacBook-Pro.local>
2014-02-04 11:50:29 +0000

    Update service templates for module dependency notation

--- templates/javascript/service/factory.js
+++ templates/javascript/service/factory.js
@@ -1,7 +1,7 @@
 define(['angular'], function (angular) {
   'use strict';
 
-  angular.module('<%= scriptAppName %>.factories')
+  angular.module('<%= scriptAppName %>.factories', [])
     .factory('<%= cameledName %>', function () {
       // Service logic
       // ...
--- templates/javascript/service/provider.js
+++ templates/javascript/service/provider.js
@@ -1,7 +1,7 @@
 define(['angular'], function (angular) {
   'use strict';
 
-  angular.module('<%= scriptAppName %>.providers')
+  angular.module('<%= scriptAppName %>.providers', [])
     .provider('<%= cameledName %>', function () {
 
       // Private variables
