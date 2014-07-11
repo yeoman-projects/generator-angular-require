6b62fac61064223882a7755f757541eefd8422f0
Aaron Allport <aaron@aaronallport.com>
2014-07-11 21:03:59 +0100

    feat(build): use bowerInstall on `watch` task

--- templates/common/Gruntfile.js
+++ templates/common/Gruntfile.js
@@ -27,6 +27,10 @@ module.exports = function (grunt) {
 
     // Watches files for changes and runs tasks based on the changed files
     watch: {
+      bower: {
+        files: ['bower.json'],
+        tasks: ['bowerInstall']
+      },
       js: {
         files: ['<%%= yeoman.app %>/scripts/{,*/}*.js'],
         tasks: ['newer:jshint:all'],
