7733b6ede4d14bee369c601fceb1ed836fae80b0
Aaron Allport <aaron@aaronallport.com>
2014-07-21 22:29:12 +0100

    fix(test): typo in generated test-main.js

--- templates/common/scripts/test-main.js
+++ templates/common/scripts/test-main.js
@@ -22,7 +22,7 @@ requirejs.config({
         'angular-cookies': ['angular']<% } %><% if (sanitizeModule) { %>,
         'angular-sanitize': ['angular']<% } %><% if (resourceModule) { %>,
         'angular-resource': ['angular']<% } %><% if (animateModule) { %>,
-        'angular-animate': ['angular']<% } %>,<% if (touchModule) { %>,
+        'angular-animate': ['angular']<% } %><% if (touchModule) { %>,
         'angular-touch': ['angular']<% } %>,
         'angular-mocks': {
           deps:['angular'],
