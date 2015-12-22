0201f6991262e56c10f388effca852615bc556de
Aaron Allport <aaron.allport@slalom.com>
2015-12-22 12:39:29 +0000

    chore(release): simplify and replace release
    https://github.com/yeoman/generator-angular/commit/c035c9ce552cdba57be30
    d1cc11d5d7d03ebc50a
    
    https://github.com/yeoman/generator-angular/commit/47c24cae3af52bd68994d
    d0dc6e31397c294f433

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,9 @@
 'use strict';
-var semver = require('semver');
 
 module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
     conventionalGithubReleaser: {
       release: {
         options: {
@@ -20,49 +18,17 @@ module.exports = function (grunt) {
         }
       }
     },
-    release: {
-      options: {
-        bump: false, // we have our own bump
-        commitMessage: 'chore(release): Release version <%= version %>',
-        file: 'package.json',
-        github: {
-          repo: 'aaronallport/generator-angular-require',
-          usernameVar: 'GITHUB_USERNAME',
-          passwordVar: 'GITHUB_AUTHTOKEN'
-        },
-        tagName: 'v<%= version %>',
-      }
+    bump: {
+      files: ['package.json'],
+      commitMessage: 'chore(release): Release version <%= version %>'
     }
   });
 
   grunt.registerTask('default', ['jshint']);
 
-  grunt.registerTask('bump', 'Bump manifest version', function (type) {
-    var options = this.options({
-      file: grunt.config('pkgFile') || 'package.json'
-    });
-
-    function setup(file, type) {
-      var pkg = grunt.file.readJSON(file);
-      var newVersion = pkg.version = semver.inc(pkg.version, type || 'patch');
-      return {
-        file: file,
-        pkg: pkg,
-        newVersion: newVersion
-      };
-    }
-
-    var config = setup(options.file, type);
-    grunt.file.write(config.file, JSON.stringify(config.pkg, null, '  ') + '\n');
-    grunt.config('pkg', config.pkg);
-    grunt.log.ok('Version bumped to ' + config.newVersion);
-  });
-
-  grunt.registerTask('stage', 'Git add files before running the release task', function () {
-    var files = this.options().files;
+  grunt.registerTask('npmPublish', 'Publish to npm', function () {
     grunt.util.spawn({
-      cmd: process.platform === 'win32' ? 'git.cmd' : 'git',
-      args: ['add'].concat(files)
+      cmd: 'npm publish'
     }, grunt.task.current.async());
   });
 
@@ -74,7 +40,7 @@ module.exports = function (grunt) {
       'default',
       'bump' + (type ? ':' + type : ''),
       'changelog',
-      'release',
+      'npmPublish',
       'conventionalGithubReleaser'
     ]);
   });
