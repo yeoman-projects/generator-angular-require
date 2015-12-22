1b7d84cb4bdc7612e04e511e0ae01cb9b94cb032
Aaron Allport <aaron.allport@slalom.com>
2015-12-22 10:08:42 +0000

    chore(build): replace load-grunt-tasks with jit-grunt
    https://github.com/yeoman/generator-angular/commit/c09e539e58e938d1961bc
    ff29102274e832604d5

--- templates/common/root/_Gruntfile.js
+++ templates/common/root/_Gruntfile.js
@@ -9,8 +9,10 @@
 
 module.exports = function (grunt) {
 
-  // Load grunt tasks automatically
-  require('load-grunt-tasks')(grunt);
+  // Automatically load required Grunt tasks
+  require('jit-grunt')(grunt, {
+    useminPrepare: 'grunt-usemin'
+  });
 
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
