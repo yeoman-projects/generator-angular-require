f68938d3f490c9f0e13e0844ced756d422afcb6e
Aaron Allport <aaron.allport@slalom.com>
2016-01-04 14:47:43 +0000

    Update route tests to latest yeoman style
    https://github.com/yeoman/generator-angular/commit/6bc529210d36d44f461d0
    ea35945e341773fbf8b

--- test/test-route-creation.js
+++ test/test-route-creation.js
@@ -7,110 +7,80 @@ var assert  = require('yeoman-assert');
 var fs      = require('fs');
 var test    = require('./helper.js');
 
-describe('Angular-RequireJS generator route mechanism', function () {
-  var route = 'simpleroute';
-  this.angularRequire = {};
+function generateFullProject(cb) {
+  helpers.run(require.resolve('../app'))
+    .withGenerators([
+      require.resolve('../controller'),
+      require.resolve('../route'),
+      require.resolve('../view'),
+      [ helpers.createDummyGenerator(), 'karma-require:app']
+    ])
+    .withOptions({
+      'appPath': 'app',
+      'skip-install': true,
+      'skip-welcome-message': true,
+      'skip-message': true
+    })
+    .withPrompts({
+      compass: true,
+      bootstrap: true,
+      compassBootstrap: true,
+      modules: ['routeModule']
+    })
+    .on('end', cb);
+}
 
-  var expected = [
-    'app/scripts/controllers/' + route + '.js',
-    'test/spec/controllers/' + route + 'Spec.js',
-    'app/views/' + route + '.html'
-  ];
-
-  var genOptions = {
-    'appPath': 'app',
-    'skip-install': true,
-    'skip-welcome-message': true,
-    'skip-message': true
-  };
-
-  var mockPrompts = {
-    compass: true,
-    bootstrap: true,
-    compassBootstrap: true,
-    modules: ['routeModule']
-  };
-
-  var deps = [
-    '../../app',
-    '../../controller',
-    '../../route',
-    '../../view', [
-      helpers.createDummyGenerator(),
-      'karma-require:app'
-    ]
-  ];
-
+describe('angular-require:route', function () {
   beforeEach(function (done) {
-    this.angularRequire = {};
+    generateFullProject(function () {
+      this.angularRequireRoute = helpers.run(require.resolve('../route'))
+        .withGenerators([
+          require.resolve('../controller'),
+          require.resolve('../view')
+        ])
+        .withOptions({
+          appPath: 'app'
+        })
+        .withArguments(['simpleroute']);
 
-    this.angularRequire.app = helpers.run(path.join(__dirname, '../app'))
-      .inDir(path.join(__dirname, 'tmp'), function () {
-        var out = [
-          '{',
-          '  "generator-angular-require": {',
-          '    "appPath": "app",',
-          '    "appName": "App"',
-          '  }',
-          '}'
-        ];
-        fs.writeFileSync('.yo-rc.json', out.join('\n'));
-      })
-      .withArguments(['App'])
-      .withOptions(genOptions)
-      .withPrompts(mockPrompts)
-      .withGenerators(deps);
+        // Hack to not clear the directory
+        this.angularRequireRoute.inDirSet = true;
 
-    done();
+        done();
+     }.bind(this));
   });
 
-  describe('create routes', function () {
-    it('should generate default route items', function(done) {
-      this.angularRequire.app
-        .on('end', function () {
-          test.createSubGenerator(
-            'route',
-            [route],
-            deps,
-            genOptions,
-            function() {
-              assert.file(expected);
-
-              assert.fileContent(
-                'app/scripts/app.js',
-                new RegExp('when\\(\'/' + route + '\'')
-              );
-
-              done();
-            }
-          );
-        });
+  it('generates default route items', function (done) {
+    this.angularRequireRoute.on('end', function () {
+      assert.file([
+        'app/scripts/controllers/simpleroute.js',
+        'test/spec/controllers/simpleroute.js',
+        'app/views/simpleroute.html'
+      ]);
+      assert.fileContent(
+        'app/scripts/app.js',
+        /when\('\/simpleroute'/
+      );
+      done();
     });
+  });
 
-    // Test with URI specified explicitly
-    it('should generate route items with the route uri given', function(done) {
-      var uri = 'segment1/segment2/:parameter';
-      genOptions["uri"] = uri;
-
-      this.angularRequire.app
-        .on('end', function () {
-          test.createSubGenerator(
-            'route',
-            [route],
-            deps,
-            genOptions,
-            function() {
-              assert.file(expected);
-
-              assert.fileContent(
-                'app/scripts/app.js',
-                new RegExp('when\\(\'/' + uri + '\'')
-              );
-
-              done();
-            }
-          );
-        });
-    });
+  it('generates route items with the route uri given', function (done) {
+    this.angularRequireRoute
+      .withOptions({
+        uri: 'segment1/segment2/:parameter'
+      })
+      .on('end', function () {
+        assert.file([
+          'app/scripts/controllers/simpleroute.js',
+          'test/spec/controllers/simpleroute.js',
+          'app/views/simpleroute.html'
+        ]);
+        assert.fileContent(
+          'app/scripts/app.js',
+          /when\('\/segment1\/segment2\/\:parameter'/
+        );
+        done();
+      });
   });
 });
